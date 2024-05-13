41b81292714c11867a64afe596407bee6c764228
Kusum Nebhwani <kusumnebhwani4@gmail.com>
2024-05-13 21:43:21 +0530

    added some changes

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ const person = require('./models/Person');
 passport.use(new LocalStrategy(async(Username,password,done)=>{
     try{
         console.log("Received Credentials: ",Username,password);
-        const user = await person.findOne({username: Username});
+        const user = await person.findOne({username});
         if(!user){
             return done(null,false,{message : "Incorrect Username"});
         }
@@ -21,6 +21,6 @@ passport.use(new LocalStrategy(async(Username,password,done)=>{
     }catch(err){
           return done(err);
     }
-}))
+}));
 
 module.exports = passport;
